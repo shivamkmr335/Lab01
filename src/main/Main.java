1aaeb6fd81f36d47d25b3cab39c443f1605c7874
shivamkmr335 <shivam.rai_cs18@gla.ac.in>
2019-08-12 15:08:03 +0530

    Main.java: add statement for printing "Hello ,..World !"

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,11 @@
 /*
  * @aurtor Shivam Kumar Rai
- * version 1.0
+ * @version 1.0
  * */
 package main;
 
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("Heloo,..World !");
+    }
 }
